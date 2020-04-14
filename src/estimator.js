4d9bfc2e00ae4473427c4153d54ea58b7e018515
dansbrainy <47524893+dansbrainy@users.noreply.github.com>
2020-04-15 00:12:21 +0200

    MainChanges

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,51 @@
 const covid19ImpactEstimator = (data) => data;
 
+  data = {
+    region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 4,
+        avgDailyIncomePopulation: 0.73
+    },
+        periodType: "days",
+        timeToElapse: 38,
+        reportedCases:2747,
+        population: 92931687,
+        totalHospitalBeds: 678874
+    },
+    estimate = {
+        impact: {
+            currentlyInfected : function() {
+                reportedCases * 10;
+                },
+            infectionsByRequestedTime : function() {
+                days = impact.currentlyInfected * 512;
+                weeks = days/7;
+                months = days/30;
+            },
+            severeCasesByRequestedTime : function(){
+                return impact.infectionsByRequestedTime * 0.15;
+            }
+            },
+        severeImpact: {
+            currentlyInfected : function() {
+                reportedCases * 50;
+            },
+            infectionsByRequestedTime : function(){
+                days = severeImpact.currentlyInfected * 512;
+                weeks = days/7;
+                months = days/30;
+            },
+            severeCasesByRequestedTime : function(){
+                return severeImpact.infectionsByRequestedTime * 0.15;
+            },
+            hospitalBedsByRequestedTime : function(){
+                return totalHospitalBeds - severeImpact.severeCasesByRequestedTime;
+            }
+        },
+    }
+
+}
+
 export default covid19ImpactEstimator;
+
